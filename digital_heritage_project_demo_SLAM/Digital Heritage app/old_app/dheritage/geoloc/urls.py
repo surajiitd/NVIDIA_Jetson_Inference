f495f033f34f952e22fe588f094d46bbf4496ad7
Aryan Singh <aryan7109singh@gmail.com>
2024-04-12 15:31:10 +0530

    Cleanup Webapp folders

--- digital_heritage_project_demo_SLAM/Digital Heritage app/old_app/dheritage/geoloc/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import include, path
-
-from . import views
-
-urlpatterns = [
-    path('', views.index, name='index'),
-    path('locations/', views.locations_list, name='list'),
-    path('updatecurrent/', views.update_current_location, name='update'),
-]
